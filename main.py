e97c39ba463c1345296c3b5ee8d9beab35d12575
s-kate <ksafronyuk07@gmail.com>
2023-06-29 19:00:19 +0300

    correct pi

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ class Circle:
 
     def __init__(self, radius):
         self.radius = radius
-        self.pi = math.pi
+
+    def pi(self):
+        return math.pi
 
     def area(self):
-        result = self.pi * math.sqrt(self.radius)
+        result = self.pi() * math.sqrt(self.radius)
         return math.ceil(result)
 
     def lenght(self):
-        result = 2 * self.pi * self.radius
+        result = 2 * self.pi() * self.radius
         return math.ceil(result)
 
 
