cae87d82f445029a8059eeaa647a1b2a5ee54e87
s-kate <ksafronyuk07@gmail.com>
2023-07-15 08:34:58 +0300

    correct range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def even_odd_generator(num: int):
-    i = 1
-    while num >= i:
+    for i in range(1, num+1):
         if i % 3 == 0 and i % 5 == 0:
             yield 'FizzBuzz'
         elif i % 3 == 0:
@@ -9,7 +8,6 @@ def even_odd_generator(num: int):
             yield 'Buzz'
         else:
             yield i
-        i += 1
 
 
 for el in even_odd_generator(15):
