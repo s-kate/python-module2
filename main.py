eaada205f6f73cb96ac2162dd3ca1abf26a07bfc
s-kate <ksafronyuk07@gmail.com>
2023-07-30 18:32:39 +0300

    correct even in odd

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def even_number_filter(num_list: list):
     index = -1
     while index < len(num_list)-1:
         index += 1
-        if num_list[index] % 2 == 0:
+        if num_list[index] % 2 != 0:
             yield num_list[index]
 
 
