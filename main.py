2b0617796e57f34a7e4cf95a9f45fd25bab4ec48
s-kate <ksafronyuk07@gmail.com>
2023-08-11 13:17:18 +0300

    clear

--- main.py
+++
@@ -1,8 +0,0 @@
-class LoggingMeta(type):
-    def __init__(cls, name, bases, attrs):
-        print(f'Class {name} has been created')
-        super().__init__(name, bases, attrs)
-
-
-class MyClass(metaclass=LoggingMeta):
-    pass
