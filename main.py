8d4a9367c6e740e7fc9b8723e5c17fa02c54119e
s-kate <ksafronyuk07@gmail.com>
2023-08-12 13:08:43 +0300

    c

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class NoDunderAttributes(type):
-    def __init__(cls, name, bases, attrs):
+    def __new__(cls, name, bases, attrs):
         for attr_name in attrs:
             if attr_name[0] == '_' and attr_name[1] == '_':
                 raise Exception('Cannot have attribute names starting with "__"')
-        super().__init__(name, bases, attrs)
+        return super().__new__(name, bases, attrs)
 
 
 class MyPrivateClass(metaclass=NoDunderAttributes):
